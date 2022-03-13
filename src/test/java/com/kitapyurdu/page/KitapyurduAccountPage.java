2a3fb8d4f9437ddf413b71d348207471ecccc79b
manevra0 <alikayakornomer@gmail.com>
2022-03-13 13:00:53 +0300

    This commit contaion the main Testinium Selenium project details.

--- src/test/java/com/kitapyurdu/page/KitapyurduAccountPage.java
+++ src/test/java/com/kitapyurdu/page/KitapyurduAccountPage.java
@@ -3,6 +3,7 @@ package com.kitapyurdu.page;
 import com.kitapyurdu.methods.Methods;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.util.Assert;
 import org.openqa.selenium.By;
 
 public class KitapyurduAccountPage {
@@ -16,14 +17,15 @@ public class KitapyurduAccountPage {
 
         methods.click(By.id("search-input"));
         methods.sendKeys(By.id("search-input"), "Oyuncak");
-        methods.waitBySeconds(1);
+        methods.isElementVisible(By.id("search-input"));
         methods.click(By.cssSelector(".common-sprite.button-search"));
-        methods.waitBySeconds(3);
+        methods.isElementVisible(By.cssSelector(".common-sprite.button-search"));
         methods.scrollWithAction(By.id("faceted-search-group-6"));
-        methods.waitBySeconds(3);
+        methods.isElementVisible(By.id("faceted-search-group-6"));
         methods.addFavorites();
-        methods.waitBySeconds(3);
+        methods.waitBySeconds(2);
         methods.addFavoritesControl();
+        methods.waitBySeconds(2);
         methods.click(By.xpath("//div[@class='logo-icon']"));
         logger.info("Ürünler Favoritelere eklendi");
 
